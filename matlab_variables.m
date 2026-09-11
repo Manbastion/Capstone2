947e5877325ebebaac30098b26feb80143d02bb8
tarushsrivastava1 <tarushtwisha@gmail.com>
2026-09-11 11:46:51 +0930

    Include, Exclude, and Everything attribute added

--- matlab_variables.m
+++ matlab_variables.m
@@ -1,7 +1,7 @@
-variable_a = 2.0
-variable_b = 4.0
-variable_c = 6.0
-variable_d = 8.0
-variable_e = 10.0
-variable_f = 12.0
-variable_g = 14.0
+variable_a = 0.0
+variable_b = 0.0
+variable_c = 0.0
+variable_d = 0.0
+variable_e = 0.0
+variable_f = 0.0
+variable_g = 0.0
--- python_variables.py
+++ python_variables.py
@@ -1,6 +1,6 @@
-variable_a = 2.0
-variable_b = 4.0
-variable_c = 6.0
-variable_d = 8.0
-variable_e = 10.0
-variable_f = 12.0
+variable_a = 0.0
+variable_b = 0.0
+variable_c = 1.0
+variable_d = 1.0
+variable_e = 1.0
+variable_f = 1.0
